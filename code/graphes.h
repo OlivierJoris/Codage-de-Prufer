c5ec4f55d535643596fdf08a789214de39960c66
Maxime Goffart <maximegoffart@icloud.com>
2019-10-08 18:57:26 +0200

    Correction indetation graphes.c

--- code/graphes.h
+++ code/graphes.h
@@ -1,31 +1,30 @@
-#if defined(GRAPHES_H)
-#else
+#ifndef GRAPHES_H
 #define GRAPHES_H
 
 #define MAX 10000
 
-struct eltadj {
-	int dest;
-	int info;            
-	struct eltadj *suivant;
+struct eltadj{ //Liste d'adjacence -- Sommet adjacent à un somme donné.
+	int dest; //Extrémité de l'arc.
+	int info; //Quelconque info - poids par exemple.
+	struct eltadj *suivant; //Sommet adjacent suivant.
 };
 
-struct sommet {
-	int label;
-	int info;    
-	struct sommet *suivant;      
-	struct eltadj *adj;  
+struct sommet{ //Un sommet du graphe.
+  int label; //Indice du sommet.
+	int info; //Info propre à un sommet. ie : Déjà visité ou couleur.
+	struct sommet *suivant; //Sommet suivant dans la liste de tout les sommets.
+	struct eltadj *adj; //Sommets adjacents à un sommet donné.
 };
 
-struct graphe {
-	int nbS;          
-	int nbA;         
-	int maxS;        
-	struct sommet *premierSommet;
-	struct sommet *dernierSommet; 
+struct graphe{
+	int nbS; //Nombre de sommets dans le graphe.
+	int nbA; //Nombre d'arcs dans le graphe.
+	int maxS; //Indice max attribué à un sommet.
+	struct sommet *premierSommet; //Pt vers le premier sommet de la liste de sommets.
+	struct sommet *dernierSommet; //Pt vers le dernier sommet de la liste de sommets.
 };
 
-typedef struct graphe GRAPHE; 
+typedef struct graphe GRAPHE;
 typedef struct sommet SOMMET;
 typedef struct eltadj ELTADJ;
 
--- code/main.c
+++ code/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv){
+#include "graphes.h"
+
+int main(/*int argc, char** argv*/void){
 
   printf("It's working.\n");
+
+	//Exemple
+	GRAPHE g;
+	lireFichier("test.txt", &g);
+	afficherGraphe(&g);
+	supprimerGraphe(&g);
 
   return 0;
 }
